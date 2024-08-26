b1a9aa6458cad7de8787a81e57f995d68db2f5a4
Pradeep Thakur <43206415+pradeepthakur-lab@users.noreply.github.com>
2024-08-26 12:52:15 +0530

    Added: Now playing movies and popular movies list

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,9 +3,11 @@ import Header from "./Header";
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
 import MainContainer from './MainContainer';
 import SecondryContainer from './SecondryContainer';
+import usePopularMovies from '../hooks/usePopularMovies';
 
 const Browse = () => {
   const movies = useNowPlayingMovies();
+  const popularMovies = usePopularMovies();
 
   return (
     <div>
--- src/components/SecondryContainer.js
+++ src/components/SecondryContainer.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import MovieList from './MovieList';
+
 
 const SecondryContainer = () => {
+  const movies = useSelector((store) => store.movies);
   return (
-    <div>SecondryContainer</div>
+     <div className="bg-black">
+        <div className=" mt-0 md:-mt-50 pl-4 md:pl-12 relative z-20">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies.popularMovies} />
+          <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        </div>
+      </div>
   )
 }
 
-export default SecondryContainer
+export default SecondryContainer;
